ac9d23678a7a18b598c73feeb63646b537dc3325
George Nicoll <george.nicoll@gmail.com>
2025-10-05 21:07:07 +0100

    Generate a better summary

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ use hickory_proto::op::{Header, ResponseCode};
 use hickory_proto::rr::rdata::CNAME;
 use hickory_proto::rr::{LowerName, Name, RData, Record, RecordType};
 use hickory_server::authority::MessageResponseBuilder;
-use hickory_server::server::{Request, RequestHandler, ResponseHandler, ResponseInfo, ServerFuture};
+use hickory_server::server::{
+    Request, RequestHandler, ResponseHandler, ResponseInfo, ServerFuture,
+};
 use serde::{Deserialize, Deserializer};
 use tokio::net::UdpSocket;
-
 
 fn deserialize_regex<'de, D>(deserializer: D) -> Result<regex::Regex, D::Error>
 where
@@ -26,7 +27,6 @@ struct Replacement {
     to: String,
 }
 
-
 #[derive(Deserialize)]
 struct Config {
     bind_address: String,
@@ -35,10 +35,9 @@ struct Config {
 
 impl Config {
     fn load_from_json(json: &str) -> Result<Self> {
-        serde_json::from_str(json).map_err(|e| e.into())
+        serde_json::from_str(json).map_err(Into::into)
     }
 }
-
 
 #[derive(Clone)]
 struct DomainConversionHandler {
@@ -53,10 +52,16 @@ impl DomainConversionHandler {
     fn find_replacement(&self, name: &LowerName) -> Option<String> {
         self.replacements.iter().find_map(|replacement| {
             replacement.from.captures(&name.to_utf8()).map(|caps| {
-                let vars: HashMap<String, String> = caps.iter().enumerate().fold(HashMap::new(), |mut map, (index, cap)| {
-                    map.insert(index.to_string(), cap.map_or_else(String::new, |c| c.as_str().to_string()));
-                    map
-                });
+                let vars: HashMap<String, String> =
+                    caps.iter()
+                        .enumerate()
+                        .fold(HashMap::new(), |mut map, (index, cap)| {
+                            map.insert(
+                                index.to_string(),
+                                cap.map_or_else(String::new, |c| c.as_str().to_string()),
+                            );
+                            map
+                        });
                 strfmt::strfmt(&replacement.to, &vars).unwrap()
             })
         })
@@ -65,19 +70,18 @@ impl DomainConversionHandler {
 
 #[async_trait::async_trait]
 impl RequestHandler for DomainConversionHandler {
-
     async fn handle_request<R: ResponseHandler>(
         &self,
         request: &Request,
         mut response_handle: R,
     ) -> ResponseInfo {
         // Check if the first query matches something we can handle
-        let record =  if let Some(query) = request.queries().first() {
+        let record = request.queries().first().and_then(|query| {
             match query.query_type() {
                 RecordType::A | RecordType::AAAA | RecordType::ANY => {
                     // Try to match the name against a replacement
                     let new_value = self.find_replacement(query.name());
-                    new_value.map( |value| {
+                    new_value.map(|value| {
                         // Respond with a CNAME record pointing to the new value
                         Record::from_rdata(
                             query.name().into(),
@@ -85,16 +89,13 @@ impl RequestHandler for DomainConversionHandler {
                             RData::CNAME(CNAME(Name::from_utf8(value).unwrap())),
                         )
                     })
-                },
+                }
                 _ => None,
             }
-        } else {
-            None
-        };
+        });
 
         // Send the response
-        if record.is_some() {
-            let rec = record.unwrap();
+        if let Some(rec) = record {
             let mr = MessageResponseBuilder::from_message_request(request).build(
                 Header::response_from_request(request.header()),
                 vec![&rec],
@@ -105,17 +106,16 @@ impl RequestHandler for DomainConversionHandler {
             response_handle.send_response(mr).await.unwrap()
         } else {
             // No replacement found, respond with NXDomain
-            let mr = MessageResponseBuilder::from_message_request(request).error_msg(
-                request.header(),
-                ResponseCode::NXDomain
-            );
+            let mr = MessageResponseBuilder::from_message_request(request)
+                .error_msg(request.header(), ResponseCode::NXDomain);
             response_handle.send_response(mr).await.unwrap()
         }
     }
-
 }
 
-async fn create_server(config: Config) -> Result<ServerFuture<DomainConversionHandler>, Box<dyn std::error::Error>> {
+async fn create_server(
+    config: Config,
+) -> Result<ServerFuture<DomainConversionHandler>, Box<dyn std::error::Error>> {
     // Bind to UDP port 8053 (you can change this)
     let socket = UdpSocket::bind(config.bind_address).await?;
 
@@ -126,36 +126,42 @@ async fn create_server(config: Config) -> Result<ServerFuture<DomainConversionHa
     Ok(server)
 }
 
-
 #[derive(Parser, Debug)]
 #[command(name = "dns-redirect")]
-#[command(about = "A simple DNS server that redirects queries based on regex replacements using cname records.")]
+#[command(
+    about = "A simple DNS server that redirects queries based on regex replacements using cname records."
+)]
 struct Args {
-    #[arg(short, long, value_name = "FILE", default_value = "config.json", help = "Path to the JSON configuration file.")]
+    #[arg(
+        short,
+        long,
+        value_name = "FILE",
+        default_value = "config.json",
+        help = "Path to the JSON configuration file."
+    )]
     config_file: String,
 }
 
-
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Args::command().print_help()?;
 
     let args = Args::parse();
-    println!("");
+    println!();
     println!("Using config file: {}", args.config_file);
-    println!("");
+    println!();
     let json = std::fs::read_to_string(args.config_file)?;
     let config = Config::load_from_json(&json)?;
 
-    println!("");
+    println!();
     println!("Starting server on {} ...", &config.bind_address);
-    println!("");
+    println!();
 
     let mut server = create_server(config).await?;
 
-    println!("");
+    println!();
     println!("Server Running on ...");
-    println!("");
+    println!();
 
     // Run the server
     server.block_until_done().await?;
@@ -172,11 +178,20 @@ mod tests {
     use futures::stream::{self, StreamExt};
     use hickory_proto::rr::{RData, Record, RecordType};
     use hickory_proto::xfer::Protocol;
+    use hickory_resolver::Resolver;
     use hickory_resolver::config::{NameServerConfig, ResolverConfig, ResolverOpts};
     use hickory_resolver::name_server::TokioConnectionProvider;
-    use hickory_resolver::Resolver;
 
     use super::*;
+
+    impl Config {
+        fn new(bind_address: String, replacements: Vec<Replacement>) -> Self {
+            Self {
+                bind_address,
+                replacements,
+            }
+        }
+    }
 
     fn find_free_port() -> std::io::Result<u16> {
         let socket = std::net::UdpSocket::bind("127.0.0.1:0")?;
@@ -185,39 +200,40 @@ mod tests {
     }
 
     fn setup_resolver(server_address: &str) -> Resolver<TokioConnectionProvider> {
-        let name_server_config = NameServerConfig::new(
-            SocketAddr::from_str(server_address).unwrap(),
-            Protocol::Udp,
-        );
+        let name_server_config =
+            NameServerConfig::new(SocketAddr::from_str(server_address).unwrap(), Protocol::Udp);
 
         let mut resolver_config = ResolverConfig::new();
         resolver_config.add_name_server(name_server_config);
 
-        let mut  resolver_opts = ResolverOpts::default();
+        let mut resolver_opts = ResolverOpts::default();
         resolver_opts.recursion_desired = false;
 
-        Resolver::builder_with_config(
-            resolver_config,
-            TokioConnectionProvider::default()
-        )
-        .with_options(resolver_opts)
-        .build()
+        Resolver::builder_with_config(resolver_config, TokioConnectionProvider::default())
+            .with_options(resolver_opts)
+            .build()
     }
 
     async fn test_server(replacements: Vec<Replacement>, test_cases: Vec<(&str, &str)>) {
         let address = format!("127.0.0.1:{}", find_free_port().unwrap());
-        let mut server = create_server(Config::new(address.clone(), replacements)).await.unwrap();
+        let mut server = create_server(Config::new(address.clone(), replacements))
+            .await
+            .unwrap();
 
         let resolver = setup_resolver(&address);
 
         let stream = stream::iter(test_cases.iter());
 
         // Perform lookups
-        let lookup_results = stream.fold(vec![], |mut acc, (query, _)| async {
-            let result = resolver.lookup(Name::from_utf8(*query).unwrap(), RecordType::ANY).await;
-            acc.push(result);
-            acc
-        }).await;
+        let lookup_results = stream
+            .fold(vec![], |mut acc, (query, _)| async {
+                let result = resolver
+                    .lookup(Name::from_utf8(*query).unwrap(), RecordType::ANY)
+                    .await;
+                acc.push(result);
+                acc
+            })
+            .await;
 
         server.shutdown_gracefully().await.unwrap();
 
@@ -230,110 +246,135 @@ mod tests {
                 }
             };
 
-            let is_matching_cname = |record: &Record| {
-                match record.data() {
-                    RData::CNAME(cname) => cname.to_string() == *expected,
-                    _ => false,
-                }
+            let is_matching_cname = |record: &Record| match record.data() {
+                RData::CNAME(cname) => cname.to_string() == *expected,
+                _ => false,
             };
 
-            assert!(result.records().iter().any(is_matching_cname), "Didn't find {} in the cnames", *expected);
+            assert!(
+                result.records().iter().any(is_matching_cname),
+                "Didn't find {} in the cnames",
+                *expected
+            );
         }
     }
 
     #[tokio::test]
     async fn test_single_mapping_returns_expected_cname() {
-        test_server(vec![
-            Replacement {
+        test_server(
+            vec![Replacement {
                 from: regex::Regex::new(r"^.*$").unwrap(),
                 to: "bob.lan.".to_string(),
-            }
-        ],
-        vec![
-            ("bob.mnh", "bob.lan."),
-            ("alice.mnh", "bob.lan."),
-            ("charlie.pod", "bob.lan."),
-        ]).await;
+            }],
+            vec![
+                ("bob.mnh", "bob.lan."),
+                ("alice.mnh", "bob.lan."),
+                ("charlie.pod", "bob.lan."),
+            ],
+        )
+        .await;
     }
 
     #[tokio::test]
     async fn test_mapping_returns_expected_cnames() {
-        test_server(vec![
-            Replacement {
+        test_server(
+            vec![Replacement {
                 from: regex::Regex::new(r"^(.*)\.mnh.?$").unwrap(),
                 to: "{1}.lan.".to_string(),
-            }
-        ],
-        vec![
-            ("bob.mnh", "bob.lan."),
-            ("alice.mnh", "alice.lan."),
-            ("big.site.mnh", "big.site.lan."),
-        ]).await;
+            }],
+            vec![
+                ("bob.mnh", "bob.lan."),
+                ("alice.mnh", "alice.lan."),
+                ("big.site.mnh", "big.site.lan."),
+            ],
+        )
+        .await;
     }
 
     #[tokio::test]
     async fn test_multiple_replacements() {
-        test_server(vec![
-            Replacement {
-                from: regex::Regex::new(r"^(.*)\.mnh.?$").unwrap(),
-                to: "{1}.lan.".to_string(),
-            },
-            Replacement {
-                from: regex::Regex::new(r"^(.*)\.(.*)\.pod.?$").unwrap(),
-                to: "{2}.{1}.pod.".to_string(),
-            },
-        ],
-        vec![
-            ("bob.mnh", "bob.lan."),
-            ("alice.chad.pod", "chad.alice.pod."),
-            ("big.site.mnh", "big.site.lan."),
-            ("x.y.z.pod", "z.x.y.pod."), // Matching is greedy
-        ]).await;
+        test_server(
+            vec![
+                Replacement {
+                    from: regex::Regex::new(r"^(.*)\.mnh.?$").unwrap(),
+                    to: "{1}.lan.".to_string(),
+                },
+                Replacement {
+                    from: regex::Regex::new(r"^(.*)\.(.*)\.pod.?$").unwrap(),
+                    to: "{2}.{1}.pod.".to_string(),
+                },
+            ],
+            vec![
+                ("bob.mnh", "bob.lan."),
+                ("alice.chad.pod", "chad.alice.pod."),
+                ("big.site.mnh", "big.site.lan."),
+                ("x.y.z.pod", "z.x.y.pod."), // Matching is greedy
+            ],
+        )
+        .await;
     }
 
     #[tokio::test]
     async fn test_no_match_returns_nxdomain() {
         let address = format!("127.0.0.1:{}", find_free_port().unwrap());
-        let mut server = create_server(Config::new(address.clone(), vec![
-            Replacement {
+        let mut server = create_server(Config::new(
+            address.clone(),
+            vec![Replacement {
                 from: regex::Regex::new(r"^(.*)\.mnh.?$").unwrap(),
                 to: "dont.care.".to_string(),
-            }
-        ])).await.unwrap();
+            }],
+        ))
+        .await
+        .unwrap();
 
         let resolver = setup_resolver(&address);
 
-        let lookup_result = resolver.lookup(Name::from_utf8("barry.net").unwrap(), RecordType::ANY).await;
+        let lookup_result = resolver
+            .lookup(Name::from_utf8("barry.net").unwrap(), RecordType::ANY)
+            .await;
 
         server.shutdown_gracefully().await.unwrap();
 
         // Check we got the expected error
         match lookup_result {
             Ok(res) => panic!("Expected NXDomain but got result: {:?}", res),
-            Err(e) => assert!(e.is_nx_domain(), "Expected NXDomain but got different error: {}", e),
+            Err(e) => assert!(
+                e.is_nx_domain(),
+                "Expected NXDomain but got different error: {}",
+                e
+            ),
         };
     }
 
     #[tokio::test]
     async fn test_wrong_query_type_returns_nxdomain() {
         let address = format!("127.0.0.1:{}", find_free_port().unwrap());
-        let mut server = create_server(Config::new(address.clone(), vec![
-            Replacement {
+        let mut server = create_server(Config::new(
+            address.clone(),
+            vec![Replacement {
                 from: regex::Regex::new(r"^(.*)\.net.?$").unwrap(),
                 to: "dont.care.".to_string(),
-            }
-        ])).await.unwrap();
+            }],
+        ))
+        .await
+        .unwrap();
 
         let resolver = setup_resolver(&address);
 
-        let lookup_result = resolver.lookup(Name::from_utf8("barry.net").unwrap(), RecordType::CSYNC).await;
+        let lookup_result = resolver
+            .lookup(Name::from_utf8("barry.net").unwrap(), RecordType::CSYNC)
+            .await;
 
         server.shutdown_gracefully().await.unwrap();
 
         // Check we got the expected error
         match lookup_result {
             Ok(res) => panic!("Expected NXDomain but got result: {:?}", res),
-            Err(e) => assert!(e.is_nx_domain(), "Expected NXDomain but got different error: {}", e),
+            Err(e) => assert!(
+                e.is_nx_domain(),
+                "Expected NXDomain but got different error: {}",
+                e
+            ),
         };
     }
 
@@ -381,5 +422,4 @@ mod tests {
         assert!(config.replacements[0].from.is_match("bob.mnh"));
         assert_eq!(config.replacements[0].to, "{1}.lan.");
     }
-
 }
